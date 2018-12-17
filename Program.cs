496d43018f104454ed1413227aa18e39485f670b
Sergio Luis Para <sluis01@ucm.es>
2018-12-17 20:49:16 +0100

    Add basic project structure
    The StockMarketService simulates a cryptocurrency market API that
    triggers an event with the updates of the cryptos and their price.
    The Program just adds a handler to the event and logs them through
    console.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ucm.Mii.Pdap.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CryptocurrencyMarketService market = new CryptocurrencyMarketService();
+
+            // You should NEVER register EventHandlers defining them as
+            // anonymous functions, as you loose the function's ref and thus you
+            // can't unregister it when needed.
+            // For this dummy example is OK.
+            market.StockUpdate += (sender, e) => 
+            {
+                Console.WriteLine(
+                    $"{e.Update.Currency} -> {e.Update.Price} USD.");
+            };
+
+            // We only connect the CryptocurrencyMarket service once the event
+            // handler is in place, as we don't want to miss events.
+            market.Connect();
+
+            // Wait for key press to exit.
+            Console.ReadLine();
+
+            market.Disconnect();
+        }
+    }
+}
---
+++ StockMarketService.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Ucm.Mii.Pdap.Presentation
+{
+    public class CryptocurrencyMarketService
+    {
+        public delegate void MarketUpdateHandler(
+            object sender, MarketUpdateEventArgs e);
+
+        public event MarketUpdateHandler StockUpdate;
+
+        public CryptocurrencyMarketService()
+        {
+            mRandom = new Random(Environment.TickCount);
+            mActualPrices = new Dictionary<string, double>();
+
+            List<string> cryptoCurrencies = new List<string>()
+            {
+                "Bitcoin", "XRP", "Ethereum", "Tether", "Stellar", "EOS"
+            };
+
+            cryptoCurrencies.ForEach(cryptoName =>
+                mActualPrices.Add(cryptoName, 10.0 + mRandom.Next(-2, 3)));
+        }
+
+        public void Connect()
+        {
+            lock (mSyncLock)
+            {
+                mbIsRunning = true;
+                ThreadPool.QueueUserWorkItem((state) => { BackgroundJob(); });
+            }
+        }
+
+        public void Disconnect()
+        {
+            lock (mSyncLock)
+            {
+                mbIsRunning = false;
+            }
+        }
+
+        public bool IsConnected()
+        {
+            lock (mSyncLock)
+            {
+                return mbIsRunning;
+            }
+        }
+
+        void BackgroundJob()
+        {
+            while (true)
+            {
+                // 1.- Choose random cryptocurrency.
+                string chosenCrypto = mActualPrices.Keys.ElementAt(
+                    mRandom.Next(0, mActualPrices.Count));
+
+                // 2.- Calculate price delta (based on probability!).
+                double delta = (mRandom.Next(0, 10) == 0)
+                    ? 5.0
+                    : mRandom.Next(1, 4);
+
+                delta *= (mRandom.Next(0, 2) == 1) ? -1 : 1;
+
+                // 3.- Update price on the dictionary.
+                double oldPrice = mActualPrices[chosenCrypto];
+                double newPrice = oldPrice + delta;
+                if (newPrice < 0)
+                    newPrice = 0;
+
+                mActualPrices[chosenCrypto] = newPrice;
+
+                // 4.- Trigger the event.
+                MarketUpdateEventArgs args =
+                    new MarketUpdateEventArgs(
+                        new CryptoUpdate(
+                            chosenCrypto, mActualPrices[chosenCrypto]));
+
+                // The safest way to call an event handler is copying its reference
+                // first to a temporal variable.
+                // The actual handler could get removed between the nullity check
+                // and the invoke if we do it this way:
+                //
+                // if (StockUpdate != null)
+                //      StockUpdate(this, args);
+                MarketUpdateHandler temp = StockUpdate;
+                temp?.Invoke(this, args);
+
+                // 5.- Check if the server must keep running.
+                if (!IsConnected())
+                    return;
+
+                // 6.- Sleep a random amount of time.
+                Thread.Sleep(mRandom.Next(250, 1250));
+            }
+        }
+
+        readonly Random mRandom;
+        readonly Dictionary<string, double> mActualPrices;
+
+        bool mbIsRunning = false;
+        object mSyncLock = new object();
+    }
+
+    public class CryptoUpdate
+    {
+        public readonly string Currency;
+        public readonly double Price;
+        public CryptoUpdate(string currency, double price)
+        {
+            Currency = currency;
+            Price = price;
+        }
+    }
+
+    public class MarketUpdateEventArgs : EventArgs
+    {
+        public readonly CryptoUpdate Update;
+        public MarketUpdateEventArgs(CryptoUpdate update)
+        {
+            Update = update;
+        }
+    }
+}
